bae092e0311720a0d429a5e0f80c965ba82bfa2c
Wisleymiguel <wisleymiguel4032x@gmail.com>
2024-12-07 18:05:08 -0300

    primeiro teste automatizado 07/12/2024

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+  
+    it('Deve fazer login com sucesso', () => {
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
+      cy.get('#username').type('wisley.teste@teste.com.br')
+      cy.get('#password').type('teste@123')
+      cy.get('.woocommerce-form > .button').click()
+      
+      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should ('contain', 'Olá, wisley.teste (não é wisley.teste? Sair)')
+    });
+  
+  });
+  
